689d4fca2a36fd60904ff2a3f66fcde5401a078f
b_babaesh <bazlajter30@gmail.com>
2025-05-28 08:41:40 +0300

    fix: build error fixes

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,9 +1,10 @@
 import ComponentWithChildren from "@/utils/types/componentWithChildren";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Авторизация",
 };
 
 export default function RootLayout({ children }: ComponentWithChildren) {
-  return children;
+  return <Suspense>{children}</Suspense>;
 }
